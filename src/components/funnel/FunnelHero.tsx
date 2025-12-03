ddee09c92912a6d6b2f2f957f02d63bb7e0a2504
Santiago Galardi <santygalardi@gmail.com>
2025-12-03 17:42:16 -0300

    bunnynetvideo2

--- src/components/funnel/FunnelHero.tsx
+++ src/components/funnel/FunnelHero.tsx
@@ -45,7 +45,7 @@ const FunnelHero = ({ type }: FunnelHeroProps) => {
       <div className="max-w-4xl mx-auto text-center mt-6 mb-6">
         <div className="relative w-full pb-[50%] h-0 overflow-hidden rounded-xl shadow-lg bg-black">
           <iframe
-            src="https://iframe.mediadelivery.net/embed/554669/723e8a02-487a-4ad6-849e-612d20ba231c?autoplay=false&loop=false&muted=false&controls=false"
+            src="https://iframe.mediadelivery.net/embed/554669/723e8a02-487a-4ad6-849e-612d20ba231c?autoplay=true&loop=false&muted=true&controls=false"
             loading="lazy"
             className="absolute top-0 left-0 w-full h-full"
             allow="accelerometer; gyroscope; autoplay; encrypted-media; picture-in-picture;"
